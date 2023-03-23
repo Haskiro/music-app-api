8924511b6695976b0966d2a85a0c46b8888f9dc1
Павел <58857483+Haskiro@users.noreply.github.com>
2023-03-23 18:37:59 +0300

    initial commit. base generated by spring initilizr. base config

---
+++ src/main/java/com/github/haskiro/musicapp/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.github.haskiro.musicapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().permitAll();
+
+        return http.build();
+    }
+
+}
